dfc9f46927b2ab0cf6a6bb80cdb6e8db4df4d87e
Evanaellio <evan.mottais@gmail.com>
2023-09-30 18:27:27 +0200

    Add collision detection

--- scenes/game_objects/item/item.gd
+++ scenes/game_objects/item/item.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
+## /../backpack/OccupiedTilemap
+@onready var occupied_tilemap : TileMap = $"../../backpack/OccupiedTileMap"
+var tile_nodes : Array[Node2D] = []
+
+const EMPTY = Vector2i(-1, -1)
+const OCCUPIED = Vector2i(0, 0)
+
 func on_click():
 	# TODO : here, pickup the item and snap it to the mouse
 	# Might need a global (autloaded) singleton to do that and make sure only one item is selected at once
 	print("On CLICK from item tile")
+	check_occupied()
+
+func tile_ready(tile):
+	tile_nodes.append(tile)
+	
+func check_occupied():
+	for tile_node in tile_nodes:		
+		var local_coords = occupied_tilemap.to_local(tile_node.global_position)
+		var map_coords = occupied_tilemap.local_to_map(local_coords)
+		var status = occupied_tilemap.get_cell_atlas_coords(0, map_coords)
+		tile_node.set_error(status == OCCUPIED)
+			
--- scenes/game_objects/item/item_tile.gd
+++ scenes/game_objects/item/item_tile.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 # The global item (bow, hatchet, etc) linked that this tile belongs to
 @onready var item = $"../.."
+@onready var img : Sprite2D = %TilePlaceholder
 
 var clicking : bool = false
+
+
+func _ready():
+	item.tile_ready(self)
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton && event.pressed:
@@ -14,3 +19,9 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 
 func _on_area_2d_mouse_exited():
 	clicking = false
+
+func set_error(error: bool):
+	if error:
+		img.self_modulate = Color.BLACK
+	else:
+		img.self_modulate = Color.WHITE
