d03a3aea0e9478949a133a03ff5f320ff23309e1
lbonnal <lucas.bonnal@gmail.com>
2023-09-30 22:44:47 +0200

    Fix Evan's shenanigans :)

--- scenes/game_objects/item/item.gd
+++ scenes/game_objects/item/item.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 ## /../backpack/OccupiedTilemap
-@onready var occupied_tilemap : TileMap = $"../../backpack/OccupiedTileMap"
+@onready var occupied_tilemap : TileMap = $"../../OccupiedTileMap"
 var tile_nodes : Array[Node2D] = []
 
 var tiles_occupied_by_me : Array[Vector2i] = []
@@ -9,16 +9,14 @@ var tiles_occupied_by_me : Array[Vector2i] = []
 const EMPTY = Vector2i(-1, -1)
 const OCCUPIED = Vector2i(0, 0)
 
+func _ready():
+	print(occupied_tilemap)
+
 func on_click():
-	# TODO : here, pickup the item and snap it to the mouse
 	# Might need a global (autloaded) singleton to do that and make sure only one item is selected at once
-<<<<<<< HEAD
-	check_occupied()
-=======
 	print("On CLICK from item tile")
 	get_node("/root/SelectionManager").selectItem(self)
-	#check_occupied()
->>>>>>> 3af34ba67891e34961c8d5ab3bf8180c3c5e7a3b
+	check_occupied()
 
 func tile_ready(tile):
 	tile_nodes.append(tile)
@@ -32,7 +30,6 @@ func check_occupied() -> bool:
 	for tile_node in tile_nodes:
 		var map_coords = get_map_coords_for_tile_node(tile_node)
 		var status = occupied_tilemap.get_cell_atlas_coords(0, map_coords)
-<<<<<<< HEAD
 		if status == OCCUPIED:
 			return true
 	return false
@@ -51,6 +48,3 @@ func place_in_backpack() -> bool:
 		return true
 	else:
 		return false
-=======
-		tile_node.set_error(status == OCCUPIED)
->>>>>>> 3af34ba67891e34961c8d5ab3bf8180c3c5e7a3b
